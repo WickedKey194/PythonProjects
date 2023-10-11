871e21589a5649615e2763b634b17cb8123eebed
WickedKey194 <146824363+WickedKey194@users.noreply.github.com>
2023-10-11 16:49:07 +0300

    Update test.py

--- LAND/test.py
+++ LAND/test.py
@@ -1,3 +1,4 @@
+'''
 KN_B0 = """
    |___|                      /^\\
   __\_/__                    _\_/_
@@ -26,3 +27,45 @@ print("""
      
      
 input()
+'''
+
+try:
+    global counter
+    with open("data.txt", "r") as file:
+        counter = int(file.read().strip())
+except FileNotFoundError:
+    # If the file doesn't exist yet, set the counter to 0
+    #print("irnfurue")
+    counter = 0
+
+def start():
+    global counter
+    with open("data.txt", "r") as file:
+        counter = int(file.read().strip())
+    counter += 1
+    with open("data.txt", "w") as file:
+        #print("is ok")
+        file.write(str(counter))
+    
+def f1():
+    global counter
+    counter += 1
+    print(1)
+    with open("data.txt", "w") as file:
+        #print("is ok")
+        file.write(str(counter))
+
+def f2():
+    global counter
+    counter += 1
+    print(2)
+    with open("data.txt", "w") as file:
+        #print("is ok")
+        file.write(str(counter))
+        
+if counter == 0:
+    start()
+if counter == 1:
+    f1()
+if counter == 2:
+    f2()
