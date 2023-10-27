e33b043cb890fecdb8f7aaf84f0228156e7d91d0
WickedKey194 <138901241+Tudor161100@users.noreply.github.com>
2023-10-27 21:19:28 +0300

    Add files via upload

---
+++ PythonReq/PythonInstallations.bat
@@ -0,0 +1,15 @@
+python.exe -m pip install --upgrade pip
+pip install argparse
+pip install tk
+pip install os-sys
+pip install diff
+pip install subprocess32
+pip install DateTime
+pip install regex
+pip install reportlab
+pip install mplcursors
+pip install matplotlib
+pip install msvc-runtime
+pip install Pillow==9.5.0
+pip install auto-py-to-exe
+pip install pygame
